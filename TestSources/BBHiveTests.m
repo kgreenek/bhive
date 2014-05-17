08a161a34ac8c73f0466fdaa17bc13e8e844795a
Kevin Greene <kgreenek@gmail.com>
2014-05-17 02:05:13 -0700

    Renaming to BBHiveTests

---
+++ TestSources/BBHiveTests.m
@@ -0,0 +1,31 @@
+//
+//  BBHiveTests.m
+//  com.hexkvlt.bhiveTests
+//
+//  Created by Kevin Greene on 3/30/14.
+//  Copyright (c) 2014 Kevin Greene. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BBHiveTests : XCTestCase
+
+@end
+
+@implementation BBHiveTests
+
+- (void)setUp {
+  [super setUp];
+  // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+  // Put teardown code here. This method is called after the invocation of each test method in the class.
+  [super tearDown];
+}
+
+- (void)testExample {
+  // TODO: Add tests.
+}
+
+@end
