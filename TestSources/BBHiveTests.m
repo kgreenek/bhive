7edb97011ea5b6701a5dab9e05899c1d5ee157af
Kevin Greene <kgreenek@gmail.com>
2014-05-17 02:07:14 -0700

    Removing useless comments.

--- TestSources/BBHiveTests.m
+++ TestSources/BBHiveTests.m
@@ -9,18 +9,15 @@
 #import <XCTest/XCTest.h>
 
 @interface BBHiveTests : XCTestCase
-
 @end
 
 @implementation BBHiveTests
 
 - (void)setUp {
   [super setUp];
-  // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-  // Put teardown code here. This method is called after the invocation of each test method in the class.
   [super tearDown];
 }
 
